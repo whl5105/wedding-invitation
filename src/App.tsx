87a2f5dc5d3610045f4c69fa3857285b3b267012
suin <73993670+whl5105@users.noreply.github.com>
2024-06-19 21:04:10 +0900

    feat : add Fetch API data handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
 
 import styles from './App.module.scss'
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  return <div className={cx('container')}></div>
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+
+    fetch('http://localhost:8888/wedding')
+      .then((res) => {
+        if (res.ok === false) throw new Error('정보를 불러오지 못했습니다.')
+        return res.json()
+      })
+      .then((data) => setWedding(data))
+      .catch((e) => {
+        console.log('에러발생', e)
+        setError(true)
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (loading) return <FullScreenMessage type="loading" />
+  if (error) return <FullScreenMessage type="error" />
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
